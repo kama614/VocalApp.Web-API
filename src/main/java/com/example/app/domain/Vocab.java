75770d3c86e22f889c6e506059dcbe3e2ee09617
kamada <kamada@example.com>
2025-03-19 15:12:52 +0900

    domainクラス作成

---
+++ src/main/java/com/example/app/domain/Vocab.java
@@ -0,0 +1,33 @@
+// 単語情報
+package com.example.app.domain;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class Vocab {
+
+	private Integer id;
+
+	@NotBlank
+	@Size(max = 50)
+	private String word;
+
+	@NotBlank
+	@Size(max = 255)
+	private String meaning;
+
+	private VocabType vocabType;
+
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime registeredAt;
+
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime updatedAt;
+
+}
---
+++ src/main/java/com/example/app/domain/VocabType.java
@@ -0,0 +1,12 @@
+// 単語の種類
+package com.example.app.domain;
+
+import lombok.Data;
+
+@Data
+public class VocabType {
+
+	private Integer id;
+	private String name;
+
+}
